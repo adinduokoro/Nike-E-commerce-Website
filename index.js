88eef4592904a19eb904c4c9968c38d37f23a944
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-09-20 22:54:29 -0400

    Payment Screen
    Created a payment screen that also includes the customer's personal information.

--- index.js
+++ index.js
@@ -105,7 +105,23 @@ menuItems.forEach((item, index) => {
     currentProductColors.forEach((color, index) => {
       color.style.backgroundColor = choosenProduct.colors[index].code;
     })
+  })
+})
 
+currentProductColors.forEach((color,index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img
+  })
+})
+
+currentProductSizes.forEach((size,index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white"
+      size.style.color = "black"
+    })
+    size.style.backgroundColor = "black"
+    size.style.color = "white"
   })
 })
 
